2f95b04c22c116ddcc3a2e78cf1935e3f5013553
soulizs <ygj2awww@gmail.com>
2019-08-01 01:24:57 +0800

    add queue demo

--- demonstrate/race-condition/client-use-queue.js
+++ demonstrate/race-condition/client-use-queue.js
@@ -1,1 +1,42 @@
-// @TODO
+const { ajax, IO, sulisuli } = require("./base");
+
+function getFile(file) {
+  return new Promise(function(resolve) {
+    ajax(file, resolve);
+  });
+}
+
+class QueueManager {
+  queue = [];
+  isRunningQueue = false;
+
+  add(f) {
+    this.queue.push(f);
+    this.run();
+  }
+
+  async run() {
+    if (this.isRunningQueue) {
+      return;
+    } else {
+      this.isRunningQueue = true;
+    }
+
+    while (this.queue.length > 0) {
+      const item = this.queue.shift();
+      try {
+        const res = await item();
+        IO(res);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    this.isRunningQueue = false;
+  }
+}
+
+const fileQueue = new QueueManager();
+["file1", "file2", "file3"].forEach(filename => {
+  fileQueue.add(() => getFile(filename));
+});
