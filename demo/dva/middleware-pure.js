7213ad426fe46de0003450c475ee020b13ca0763
严广杰 <yanguangjie@cvte.com>
2019-12-11 13:18:09 +0800

    update mdws comments

--- demo/dva/middleware-pure.js
+++ demo/dva/middleware-pure.js
@@ -11,36 +11,63 @@ function compose(...funcs) {
 }
 
 function next(action) {
-  console.log('HANDLER next', action);
+  console.log('[next]', action);
 }
 
 const fooMdw = next => {
-  console.log("MDW -> foo next");
+  console.log('[middleware] foo next');
   return action => {
-    console.log("MDW -> foo action before");
+    console.log('[middleware] foo action before');
     next(action);
-    console.log("MDW -> foo action after");
+    console.log('[middleware] foo action after');
   };
 };
+// fooMdw.t = '[foo]';
 
 const barMdw = next => {
-  console.log("MDW -> bar next");
+  console.log('[middleware] bar next');
   return action => {
-    console.log("MDW -> bar action before");
+    console.log('[middleware] bar action before');
     next(action);
-    console.log("MDW -> bar action after");
+    console.log('[middleware] bar action after');
   };
 };
+// barMdw.t = '[bar]';
 
 const bazMdw = next => {
-  console.log("MDW -> baz next");
+  console.log('[middleware] baz next');
   return action => {
-    console.log("MDW -> baz action before");
+    console.log('[middleware] baz action before');
     next(action);
-    console.log("MDW -> baz action after");
+    console.log('[middleware] baz action after');
   };
 };
+// bazMdw.t = '[baz]';
 
+/**
+ * compose sequence
+ */
 const chain = compose(fooMdw, barMdw, bazMdw);
-// 请从函数堆栈上理解，压栈和弹栈的过程
-chain(next)('{data}');
+// compose f = (...args) => foo(bar(...args))
+// compose g = (...args) => f(baz(...args))
+// ========
+const nextChain = chain(next);
+// pass in `next` args
+// tigger mdw sequence: baz -> bar -> foo
+// ========
+// baz: next_from_baz = (action) => { ... next(action) ... };
+// bar: next_from_bar = (action) => { ... next_from_baz(action) ... };
+// foo: next_from_foo = (action) => { ... next_from_bar(action) ... };
+// ========
+// nextChain = next_from_foo
+console.log('==============================');
+nextChain('{data}');
+// dispatch action
+// trigger next_from_foo until its next -> next_from_bar
+// trigger next_from_bar until its next -> next_from_baz
+// trigger next_from_baz until its next -> next (i.e. origin next fn)
+// trigger next
+// return from next, resume next_from_baz
+// return from next_from_baz, resume next_from_bar
+// return from next_from_bar, resume next_from_foo
+// all finish
