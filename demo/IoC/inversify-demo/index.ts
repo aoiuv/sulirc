45ab4ba9a97652a442ee6de40d3a6973a9163771
严广杰 <yanguangjie@cvte.com>
2020-01-02 10:59:20 +0800

    feat: support ts demo

---
+++ demo/IoC/inversify-demo/index.ts
@@ -0,0 +1,8 @@
+import { myContainer } from "./inversify.config";
+import { TYPES } from "./types";
+import { Warrior } from "./interfaces";
+
+const ninja = myContainer.get<Warrior>(TYPES.Warrior);
+
+console.log('ninja', ninja.fight());
+console.log('ninja', ninja.fight());
---
+++ demo/IoC/main.ts
@@ -0,0 +1,1 @@
+import './inversify-demo/index';
