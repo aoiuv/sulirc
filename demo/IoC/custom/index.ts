42ca6403dbffedd125e95e3c3f647b190a2e7ef6
suli <ygj2awww@gmail.com>
2020-01-08 23:21:37 +0800

    feat: implement IoC container auto get

--- demo/IoC/custom/index.ts
+++ demo/IoC/custom/index.ts
@@ -3,20 +3,16 @@ import { Katana, Shuriken, Ninja } from './entities';
 
 type Constructor<T = any> = new (...args: any[]) => T;
 
-class Container<T> {
-  target: Constructor<T>;
+class Container {
   bindTags = {};
-  constructor(target: Constructor<T>) {
-    this.target = target;
-  }
 
   bind(tag: string | symbol, to: Constructor) {
     this.bindTags[tag] = to;
   }
 
-  get(): T {
+  get<T>(tag: string | symbol): T {
+    const target = this.bindTags[tag];
     const providers = [];
-    const target = this.target;
     for (let i = 0; i < target.length; i++) {
       const paramtypes = Reflect.getMetadata('custom:paramtypes#' + i, target);
       const provider = this.bindTags[paramtypes.value];
@@ -28,12 +24,13 @@ class Container<T> {
   }
 }
 
-const container = new Container(Ninja);
+const container = new Container();
 
 container.bind(TYPES.Weapon, Katana);
 container.bind(TYPES.ThrowableWeapon, Shuriken);
+container.bind(TYPES.Warrior, Ninja);
 
-const ninja = container.get();
+const ninja = container.get<Ninja>(TYPES.Warrior);
 
 console.log(ninja.fight());
 console.log(ninja.sneak());
