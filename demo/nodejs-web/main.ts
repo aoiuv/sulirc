b5adcca4d5e692ec06c6f090370640421b1245a1
严广杰 <yanguangjie@cvte.com>
2020-01-18 16:12:35 +0800

    add: add koa-static demo

--- demo/nodejs-web/main.ts
+++ demo/nodejs-web/main.ts
@@ -1,5 +1,8 @@
 // import './example/co';
 // import './example/koa-compose';
 // import './example/koa-router';
-import './example/koa-nest-router';
+// import './example/koa-nest-router';
+
+import './example/koa-static';
+
 // import './example/path-to-regexp';
