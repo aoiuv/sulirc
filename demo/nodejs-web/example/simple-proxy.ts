9b91b8c6582e904b92d9a8c6a4ac28f02e279aca
suli <ygj2awww@gmail.com>
2020-01-20 22:16:55 +0800

    add: add simple proxy demo

--- demo/nodejs-web/example/simple-proxy.ts
+++ demo/nodejs-web/example/simple-proxy.ts
@@ -1,12 +1,36 @@
 import http from "http";
-import fs from "fs";
+
+const debug = require("debug")("web");
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello Remote Page \n");
+  if ("/remote" === req.url) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    return res.end("Hello Remote Page \n");
+  } else {
+    proxy(req, res);
+  }
 });
 
+function proxy(req, res) {
+  const options = {
+    host: req.host,
+    port: 3000,
+    headers: req.headers,
+    path: "/remote",
+    agent: false,
+    method: "GET"
+  };
+
+  const httpProxy = http.request(options, response => {
+    debug('proxy', req.url);
+    response.pipe(res);
+  });
+
+  req.pipe(httpProxy);
+}
+
 app.listen(3000, () => {
-  const PORT = app.address().port;
-  console.log(`Server running at http://127.0.0.1:${PORT}`);
+  const address: any = app.address();
+  const PORT = address.port;
+  debug(`Server running at http://127.0.0.1:${PORT}`);
 });
