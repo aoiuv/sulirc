665377ba22ff21ba07b3fbef3d070bc5180a20cd
suli <ygj2awww@gmail.com>
2020-01-26 21:40:12 +0800

    add: add dracula ioc demo

--- demo/IoC/main.ts
+++ demo/IoC/main.ts
@@ -1,5 +1,7 @@
 // import './inversify-demo/index';
 // import './reflect-metadata/index';
 // import './reflect';
-import './custom';
-import './not-IoC';
+// import './custom';
+// import './not-IoC';
+
+import './practice/0';
