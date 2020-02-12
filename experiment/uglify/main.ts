664315eb5e432c07056a883e659fa5e7abde73dc
suli <ygj2awww@gmail.com>
2020-02-13 00:28:25 +0800

    add: show event, code length

--- experiment/uglify/main.ts
+++ experiment/uglify/main.ts
@@ -10,9 +10,25 @@ debug(CodeTable);
 const code = Object.keys(sample).map(key => {
   return compile(sample[key]);
 });
-debug('event => code', code);
+debug("event => code", code);
 
 const event = code.filter(Boolean).map(c => {
   return interpret(c);
 });
-debug('code => event', event);
+debug("code => event", event);
+
+debug(
+  "event length: ",
+  stringify({
+    actionName: "element.common.dragEnd",
+    slideId: "86069daa-a660-4f7a-b024-429e68ce4b48",
+    id: "8fd1d343-a272-4eeb-90bc-d5ac0242e5f9",
+    xAxis: "732",
+    yAxis: "275"
+  }).length
+);
+
+debug(
+  "code length: ",
+  "de:86069daa-a660-4f7a-b024-429e68ce4b48:8fd1d343-a272-4eeb-90bc-d5ac0242e5f9:732,275".length
+);
