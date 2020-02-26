c887ebbea26c5cfafa9a9bb16f356f4d5c9a1dd8
严广杰 <yanguangjie@cvte.com>
2020-02-26 18:38:00 +0800

    add pull-push-model

--- pull-push-model/main.ts
+++ pull-push-model/main.ts
@@ -1,19 +1,4 @@
-import { Observable } from 'rxjs';
-
-var observable = Observable.create(function (observer) {
-  observer.next(1);
-  observer.next(2);
-  observer.next(3);
-  setTimeout(() => {
-    observer.next(4);
-    observer.complete();
-  }, 1000);
-});
-
-console.log('just before subscribe');
-observable.subscribe({
-  next: x => console.log('got value ' + x),
-  error: err => console.error('something wrong occurred: ' + err),
-  complete: () => console.log('done'),
-});
-console.log('just after subscribe');
+// import './rxjs';
+import './function';
+import './generator';
+import './promise';
---
+++ pull-push-model/promise.ts
@@ -0,0 +1,12 @@
+function reject() {
+  const a = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject('Fake Error');
+    }, 200);
+  });
+
+  a.then(() => {}).catch(err => console.error('inside reject', err));
+  return a;
+}
+
+reject().catch(err => console.error('outside reject', err));
