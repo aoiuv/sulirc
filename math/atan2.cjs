4b2cee6829af52221f82b7cf9b74b01d9e57382b
严广杰 <yanguangjie@cvte.com>
2020-03-16 18:22:28 +0800

    add atan2

--- math/atan2.cjs
+++ math/atan2.cjs
@@ -21,7 +21,11 @@ console.log('delta:', delta);
 console.log('a, b, h', p1.x, p1.y, h);
 
 const _deg2 = deg1 + delta;
+const _x = Math.cos(degToRad(_deg2)) * h;
+const _y = Math.sin(degToRad(_deg2)) * h;
 console.log('_deg2:', _deg2);
+console.log('_x:', _x);
+console.log('_y:', _y);
 
 function degToRad(deg) {
   return (deg / 180) * Math.PI;
