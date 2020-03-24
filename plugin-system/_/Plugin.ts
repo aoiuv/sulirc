db7e286a43641efb96ed7e944a7ee92810cb0fe0
严广杰 <yanguangjie@cvte.com>
2020-03-24 11:08:31 +0800

    add: add Plugin source

--- plugin-system/_/Plugin.ts
+++ plugin-system/_/Plugin.ts
@@ -1,5 +1,44 @@
-class Plugin {
+import invariant from 'invariant';
 
+type Hook = (...args: any) => void;
+type IPluginHooks<T extends string | symbol> = Record<T, Hook[]>;
+
+class Plugin<K extends string> {
+  hooks: IPluginHooks<K>;
+
+  constructor(hooks: K[] = []) {
+    invariant(hooks.length, `Plugin hooks empty`);
+
+    this.hooks = hooks.reduce((memo, key) => {
+      memo[key] = [];
+      return memo;
+    }, {} as IPluginHooks<K>);
+  }
+
+  use(plugin: Partial<IPluginHooks<K>>) {
+    const { hooks } = this;
+    for (let key in plugin) {
+      if (Object.prototype.hasOwnProperty.call(plugin, key)) {
+        invariant(hooks[key], `plugin.use: unknown plugin property: ${key}`);
+        hooks[key] = hooks[key].concat(plugin[key]);
+      }
+    }
+  }
+
+  apply(key: K, defaultHandler: Hook = () => {}) {
+    const { hooks } = this;
+    const fns = hooks[key];
+
+    return (...args: any) => {
+      if (fns.length) {
+        for (const fn of fns) {
+          fn(...args);
+        }
+      } else {
+        defaultHandler(...args);
+      }
+    };
+  }
 }
 
 export default Plugin;
--- plugin-system/_/index.ts
+++ plugin-system/_/index.ts
@@ -1,2 +1,8 @@
 import Plugin from './Plugin';
 import TapablePlugin from './TapablePlugin';
+
+type Hook = 'onAction' | 'onError';
+
+const plugin = new Plugin<Hook>(['onAction', 'onError']);
+
+const tapablePlugin = new TapablePlugin();
