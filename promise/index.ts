20a4dcf3e3e439b50a8e75a1ae4cc72f72ca6015
严广杰 <yanguangjie@cvte.com>
2020-04-09 11:33:44 +0800

    add es6-promise polyfill

--- promise/index.ts
+++ promise/index.ts
@@ -7,7 +7,6 @@
 
 import SimplePromise from './simple-promise';
 
-
 // --------------------------------------
 
 const p1 = new SimplePromise(executor);
@@ -19,13 +18,46 @@ function executor(resolve: Function, reject: Function) {
 
 function onResolve(value: any) {
   console.log('onResolve', value);
-  return new SimplePromise(r => r(value + 1));
+  return new SimplePromise((r) => r(value + 1));
 }
 
 function onReject(error: any) {
   console.log('onReject', error);
 }
 
-const ret = p1.then(onResolve);
-console.log('ret', ret);
+// const ret = p1.then(onResolve).catch(onReject);
+// console.log('ret', ret);
 
+// ret.then(v => console.log('ret value', v));
+
+// ----------
+
+// console.log('\\\\\\\\/////////')
+const constant = v => v;
+const a1 = new Promise((resolve, reject) => {
+  // throw new TypeError('This is a TypeError');
+  setTimeout(() => {
+    resolve('This is normal');
+    reject('This is unusual');
+  });
+});
+
+const a2 = a1
+  .then(
+    (value) => {
+      console.log('a1 onResolve', value);
+      return Promise.resolve(value + ' :)');
+    },
+    (reason) => {
+      // throw new TypeError('This is a TypeError2');
+      console.log('a1 onReject', reason);
+    }
+  )
+  // .catch((err) => {
+  //   console.log('a1 onCatch', err);
+  // })
+  .then(constant);
+
+a2.then((value) => {
+  console.log('a2 onResolve', value);
+});
